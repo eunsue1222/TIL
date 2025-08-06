784c3701c6686964a32328d3f0fcc9d150e741c1
김은수 <eunsue1222@gmail.com>
2025-08-06 23:16:59 +0900

    homework

--- 04_algorithm_basic/01-algorithm-basic-1/02_homework/1209_sum/solve.py
+++ 04_algorithm_basic/01-algorithm-basic-1/02_homework/1209_sum/solve.py
@@ -1,21 +1,24 @@
-import sys
+# import sys
+# sys.stdin = open('input.txt')
 
-sys.stdin = open('input.txt') # open을 사용해서 input 파일을 연다
+T = 10
+for _ in range(T):
+    test_case = int(input())
+    grid = [list(map(int, input().split())) for _ in range(100)]
 
-for _ in range(10):
-    # 입력 받은 문자열을 공백 기준으로 쪼개서 정수로 바꾼다음 리스트에 담는걸 100번 반복
-    tc = input() # 테스트케이스 번호 입력    
-    # data = []
-    # for _ in range(100):
-    #     tmp_list = input().split()
-    #     map_data = map(int, tmp_list)
-    #     map_to_list_data = list(map_data)
-    #     data.append(map_to_list_data)
-    # print(data)
-    data = [list(map(int, input().split())) for _ in range(100)]
-    print(data)
-    
-    # 각 행마다 가진 값들을 더한다.
-    # 각 열마다 가진 값들을 더한다.
-    # 대각선의 값들을 더한다.
-    # 그 모든 값들 중 제일 큰 값을 구한다. -> max 금지
+    # 초기화
+    max_sum = 0
+    diagonal1 = 0
+    diagonal2 = 0
+
+    for i in range(100):
+        row_sum = sum(grid[i]) # 행의 합
+        col_sum = sum(grid[j][i] for j in range(100)) # 열의 합
+        max_sum = max(max_sum, row_sum, col_sum)
+
+        diagonal1 += grid[i][i] # 대각선의 합
+        diagonal2 += grid[i][99-i] # 대각선의 합
+
+    max_sum = max(max_sum, diagonal1, diagonal2) # 최대값
+
+    print(f'#{test_case} {max_sum}')
