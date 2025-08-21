f400f1e9ee415e5552594bfb432f07c58624c660
김은수 <eunsue1222@gmail.com>
2025-08-21 21:25:08 +0900

    0821 homework

--- 06_algorithm_adv/03-shortestpath/2_homework/5251_최소이동거리/solve.py
+++ 06_algorithm_adv/03-shortestpath/2_homework/5251_최소이동거리/solve.py
@@ -1,7 +1,42 @@
+# import sys
+# sys.stdin = open('sample_input.txt')
+
+import heapq
+
+def dijkstra(graph, start):
+    # 초기화
+    distances = {v: float('inf') for v in range(N+1)}
+    distances[start] = 0
+
+    heap = [] # 최소 힙
+    visited = set() # 방문 체크
+    heapq.heappush(heap, [0, start]) # [거리, 시작 정점]
+
+    while heap:
+        dist, current = heapq.heappop(heap) # [거리, 시작 정점]
+        if current in visited or distances[current] < dist:
+            continue
+
+        visited.add(current)
+        for next, weight in graph[current].items():
+            next_distance = dist + weight
+            if next_distance < distances[next]:
+                distances[next] = next_distance
+                heapq.heappush(heap, [next_distance, next])
+
+    return distances
+
+
 T = int(input())
 for test_case in range(1, T+1):
     N, E = map(int, input().split()) # N: 마지막 연결지점 번호, E: 도로의 개수
-    info = [list(map(int, input().split())) for _ in range(E)]
+    data = [list(map(int, input().split())) for _ in range(E)] # [구간 시작, 구간 끝, 구간 거리]
 
-    print(f'#{test_case} {result}')
+    # 인접 리스트
+    graph = {v:{} for v in range(N+1)}
+    for start, end, weight in data:
+        graph[start][end] = weight
 
+    result = dijkstra(graph, 0)
+
+    print(f'#{test_case} {result[N]}')
--- 06_algorithm_adv/03-shortestpath/2_homework/5263_그래프최소비용/solve.py
+++ 06_algorithm_adv/03-shortestpath/2_homework/5263_그래프최소비용/solve.py
@@ -1,7 +1,37 @@
+# import sys
+# sys.stdin = open('sample_input.txt')
+
+def floyd_warshall(graph):
+    for mid in range(N):
+        for start in range(N):
+            for end in range(N):
+                Dik = graph[start][mid]
+                Dkj = graph[mid][end]
+                Dij = graph[start][end]
+                if Dik + Dkj < Dij:
+                    graph[start][end] = Dik + Dkj
+    return graph
+
+
 T = int(input())
 for test_case in range(1, T+1):
     N = int(input()) # N: 노드의 개수
-    for _ in range(N):
-        s, e, w = map(int, input().split())
+    adj_matrix = [list(map(int, input().split())) for _ in range(N)] # [시작정점, 도착정점, 비용]
 
-    print(f'#{test_case} {result}')
+    # 초기화
+    INF = float('inf')
+    for x in range(N):
+        for y in range(N):
+            if x == y:
+                continue
+            if adj_matrix[x][y] == 0:
+                adj_matrix[x][y] = INF
+
+    result = floyd_warshall(adj_matrix)
+
+    max_value = -INF
+    for i in range(N):
+        for j in range(N):
+            max_value = max(max_value, result[i][j])
+
+    print(f'#{test_case} {max_value}')
