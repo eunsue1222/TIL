5784010836e94df0d990e18a40c28a142e923536
김은수 <eunsue1222@gmail.com>
2025-08-21 17:35:35 +0900

    0821 homework

---
+++ 06_algorithm_adv/03-shortestpath/2_homework/5263_그래프최소비용/solve.py
@@ -0,0 +1,7 @@
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input()) # N: 노드의 개수
+    for _ in range(N):
+        s, e, w = map(int, input().split())
+
+    print(f'#{test_case} {result}')
