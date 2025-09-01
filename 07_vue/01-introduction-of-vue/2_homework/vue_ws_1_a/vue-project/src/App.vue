2c4cba6809e4d26bd1b1440bc331b2dedb73e80a
김은수 <eunsue1222@gmail.com>
2025-09-01 23:15:24 +0900

    0901 homework

---
+++ 07_vue/01-introduction-of-vue/2_homework/vue_ws_1_a/vue-project/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app">
+      <MyComponent />
+    </div>
+</template>
+
+<script setup>
+import MyComponent from './components/MyComponent.vue';
+</script>
+
+<style scoped>
+</style>
---
+++ 07_vue/01-introduction-of-vue/2_homework/vue_ws_1_a/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-component">
+    <header class="header">
+      <h1>Gallery</h1>
+    </header>
+    <main class="main">
+      <div>카드</div>
+    </main>
+  </div>  
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import sample from '../assets/sample.png'
+const sampleImage = ref(sample)
+</script>
+
+<style scoped>
+</style>
