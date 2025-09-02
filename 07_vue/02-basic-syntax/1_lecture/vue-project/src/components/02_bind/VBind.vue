45ffa5cce4a6f1d8b8a756a2377c2c2ceb9d8a42
김은수 <eunsue1222@gmail.com>
2025-09-02 11:41:09 +0900

    0902 lecture

---
+++ 07_vue/02-basic-syntax/1_lecture/vue-project/src/components/02_bind/VBind.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- :[속성이름]: 동적으로 바인딩 -->
+    <img :src="imageSrc"> <!-- v-bind:src="imageSrc" -->
+    <a :href="myUrl">Move to url</a>
+    <p :[dynamicattr]="dynamicValue">Dynamic Attr</p>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const imageSrc = ref('https://picsum.photos/200')
+  const myUrl = ref('https://www.google.com/')
+  const dynamicattr = ref('title')
+  const dynamicValue = ref('Hello Vue.js')
+</script>
+
+<style scoped>
+
+</style>
