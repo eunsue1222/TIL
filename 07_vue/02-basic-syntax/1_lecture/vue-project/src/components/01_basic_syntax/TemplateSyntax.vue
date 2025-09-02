60066e1a753393c36c6ffb1a6a54f27546f4aad5
김은수 <eunsue1222@gmail.com>
2025-09-02 11:41:09 +0900

    0902 lecture

---
+++ 07_vue/02-basic-syntax/1_lecture/vue-project/src/components/01_basic_syntax/TemplateSyntax.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- template syntax -->
+    <p>Message: {{ msg }}</p>
+    <div v-html="rawHtml"></div> <!-- 사용자 입력값 v-html에 할당 금지 -->>
+    <div v-bind:id="dynamicId"></div>
+    {{ number + 1 }}
+    {{ ok ? 'YES' : 'NO' }}
+    {{ msg.split('').reverse().join('') }}
+
+    <!-- directive -->
+    <div v-bind:id="`list-${id}`"></div>
+    <p v-if="seen">Hi there</p>
+    <a v-bind:href="myUrl">Link</a>
+    <button v-on:click="doSomething">button</button>
+    <form v-on:submit.prevent="onSubmit"> <!-- prevent: 페이지 새로고침 -->
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  
+  const msg = 'Hello'
+  const rawHtml = ref('<span style="color:red">This should be red.</span>')
+  const dynamicId = ref('my-id')
+  const number = ref(1)
+  const ok = ref(true)
+  const seen = ref(true)
+  const id = ref(100)
+  const myUrl = 'https://www.google.co.kr/'
+
+  const doSomething = function () {
+    console.log('button clicked')
+  }
+  const onSubmit = function () {
+    console.log('form submitted')
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 07_vue/02-basic-syntax/1_lecture/vue-project/src/components/03_event/EventHandling.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- Inline Handlers -->
+    <button @click="count++">Add 1</button> <!-- button v-on: @ -->
+    <p>Count: {{ count }}</p>
+
+    <!-- Method Handlers -->
+    <button @click="myFunc">Hello</button>
+
+    <!-- Calling Methods in Inline Handlers -->
+    <button @click="greeting('hello')">Say hello</button>
+    <button @click="greeting('bye')">Say bye</button>
+
+    <!-- Accessing Event Argument in Inline Handlers -->
+    <button @click="warning('경고입니다.', $event)">Submit</button> <!-- $event: window 객체, 전역변수로 사용 -->
+
+    <!-- event modifiers -->
+    <form @submit.prevent="onSubmit">...</form> <!-- prevent -->
+    <a @click.stop.prevent="onLink">...</a> <!-- stop.prevent -->
+
+    <!-- key modifiers -->
+    <input @keyup.enter="onSubmit"> <!-- keyup: 눌렀다 떼는 시점 -->
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const count = ref(0)
+
+  const name = ref('Alice')
+  const myFunc = function (event) {
+    console.log(event)
+    console.log(event.currentTarget)
+    console.log(`Hello ${name.value}!`)
+  }
+
+  const greeting = function (message) {
+    console.log(message)
+  }
+  
+  const warning = function (message, event) {
+    console.log(message)
+    console.log(event)
+  }
+
+  const onSubmit = function () {
+    console.log('on Submit!!')
+  }
+</script>
+
+<style scoped>
+
+</style>
