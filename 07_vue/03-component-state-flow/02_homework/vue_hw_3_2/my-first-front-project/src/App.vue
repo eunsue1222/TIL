4b1202a9c48264c8cc04ffdb06c96fc08345f226
김은수 <eunsue1222@gmail.com>
2025-09-07 22:24:48 +0900

    0903 homework

---
+++ 07_vue/03-component-state-flow/02_homework/vue_hw_3_2/my-first-front-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-wrapper"> <!-- ✅ 가장 바깥쪽 테두리 -->
+    <ParentPage />
+  </div>
+</template>
+
+<script setup>
+import ParentPage from './components/ParentPage.vue'
+</script>
+
+<style scoped>
+.app-wrapper {
+  border: 1px solid #aaa;
+  margin: 10px;
+}
+</style>
---
+++ 07_vue/03-component-state-flow/02_homework/vue_hw_3_4/vue-project/src/components/ParentPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="outer-box">
+    <h1>부모 페이지입니다.</h1>
+    <ChildPage
+      v-for="(child, index) in children"
+      :key="index"
+      :child="child"
+      @giveMeAllowance="updateBalance"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ChildPage from './ChildPage.vue'
+
+// 자식 정보 배열
+const children = ref([
+  { name: '김하나', age: 30, balance: 102000 },
+  { name: '김두리', age: 20, balance: 12000 },
+  { name: '김서이', age: 10, balance: 4000 }
+])
+
+// 버튼 클릭 이벤트 처리
+function updateBalance(child) {
+  child.balance += 1000
+}
+</script>
+
+<style scoped>
+.outer-box {
+  border: 1px solid #999;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
