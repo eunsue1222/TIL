bb22570aab25ca82651a21c1245e207fff3d1d34
김은수 <eunsue1222@gmail.com>
2025-09-11 17:52:06 +0900

    algorithm

--- 99_suppliment/algorithm/A/3_1_컨베이어 벨트/sol.py
+++ 99_suppliment/algorithm/A/3_1_컨베이어 벨트/sol.py
@@ -1,7 +1,9 @@
 N, T = map(int, input().split())
+
 upper_nums = list(map(int, input().split()))
 lower_nums = list(map(int, input().split()))
-
 arr = upper_nums + lower_nums
 
-tmp =
+result = arr[N*2-T:] + arr[:N*2-T]
+print(*result[:N])
+print(*result[N:])
